052493ef2db2825984a1d68bd2e9210cf4f00e7b
Byron <byronmwong@gmail.com>
2018-10-22 23:00:27 -0700

    next build

--- _next/static/0bbmb6l__vDoR8GAlYnF0/pages/blog/foo.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{208:function(n,o,e){__NEXT_REGISTER_PAGE("/blog/foo",function(){return n.exports=e(209),{page:n.exports.default}})},209:function(n,o,e){"use strict";e.r(o);var t=e(1),u=e.n(t);o.default=function(){return u.a.createElement("div",null,"hello foo")}}},[[208,1,0]]]);
--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b05074fe16e004e5d2a8";
+/******/ 	var hotCurrentHash = "8b643073742be9806f9b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
