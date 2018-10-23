90ce1e7575f940a660ba0a7f8a1496f063b65d34
Byron <byronmwong@gmail.com>
2018-10-22 20:48:53 -0700

    next build

---
+++ _next/static/po0e0YO38mjXRST4vQUOu/pages/blog/foo.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{208:function(n,o,e){__NEXT_REGISTER_PAGE("/blog/foo",function(){return n.exports=e(209),{page:n.exports.default}})},209:function(n,o,e){"use strict";e.r(o);var t=e(1),u=e.n(t);o.default=function(){return u.a.createElement("div",null,"hello foo")}}},[[208,1,0]]]);
